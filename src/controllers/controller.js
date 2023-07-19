db463e76ad432f20a2b243250d8f193a3d20408a
eddieoz <osoriojr@gmail.com>
2023-07-19 23:34:52 +0300

    added relay pool (SimplePool

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,7 +2,7 @@ import { LightningAddress } from "alby-tools";
 import { webln } from "alby-js-sdk";
 import 'websocket-polyfill';
 import * as crypto from "crypto";
-import { finishEvent, getPublicKey, nip05, nip19, relayInit } from "nostr-tools";
+import { finishEvent, getPublicKey, nip05, nip19, relayInit, SimplePool } from "nostr-tools";
 import 'dotenv/config';
 
 globalThis.crypto = crypto;
@@ -49,7 +49,7 @@ export async function zapNostr(req, res) {
         } catch {
             return res.status(200).send('Please input NIP05 or Nostr npub... pubkey')
         }
-        
+
     } else {
         try {
             let nostrProfile = await nip05.queryProfile(destination)
@@ -60,7 +60,7 @@ export async function zapNostr(req, res) {
             }
         } catch {
             return res.status(200).send('Please input NIP05 or Nostr npub... pubkey')
-        }    
+        }
     }
 
     if (!dstNostrPubkey) {
@@ -68,30 +68,27 @@ export async function zapNostr(req, res) {
     }
 
     // Connect relay
-    const relay = relayInit('wss://relay.damus.io')
-    relay.on('connect', () => {
-        console.log('connected to', relay.url)
-    })
-    relay.on('error', () => {
-        console.log('failed to connect to', relay.url)
-    })
+
+    const pool = new SimplePool()
+    let relays = ['wss://nos.lol', 'wss://relay.damus.io', 'wss://relay.snort.social', 'wss://nostr.zbd.gg', 'wss://nostr.rocks', 'wss://relay.nostrplebs.com', 'wss://nostr.bitcoiner.social']
 
     let events = []
-    try{
-        await relay.connect()
-
+    try {
         // Retrieve Nostr profile
-        events = await relay.list([{ kinds: [0], authors: [dstNostrPubkey] }])
-    
+        events = await pool.list(relays, [{ kinds: [0], authors: [dstNostrPubkey] }])
+
         console.log(events)
-    
-        await relay.close()
+
     } catch {
         return res.status(200).send('Relay unavailable now. Try again later.')
     }
-    
+
+    if (!events) {
+        return res.status(200).send('Profile unavailable now. Try again later.')
+    }
+
     let content = '';
-    if (events[0].hasOwnProperty('content')){
+    if (events && events[0].hasOwnProperty('content')) {
         // Get LN Address from Nostr Profile
         content = JSON.parse(events[0].content)
     } else {
@@ -99,12 +96,12 @@ export async function zapNostr(req, res) {
     }
 
     let lnAddress = '';
-    if (content.hasOwnProperty('lud16')){
+    if (content.hasOwnProperty('lud16')) {
         lnAddress = content.lud16.toString().trim()
     } else {
         return res.status(200).send('Lightning Address not found in this Nostr Profile')
     }
-    
+
 
     // Validate LN Address
     const ln = new LightningAddress(lnAddress, {
@@ -126,29 +123,36 @@ export async function zapNostr(req, res) {
         return res.status(200).send('Nostr pubkey not found')
     }
 
-    
+
     // Prepare zap
     const zapArgs = {
         satoshi: sats,
-        comment: "Morning Crypto: Blokitos <> Sats",
+        comment: `Morning Crypto: ${sats} blokitos <> ${sats} sats`,
         relays: ["wss://relay.damus.io", "wss://relay.snort.social", "wss://nostr.mom", "wss://nos.lol", "wss://nostr.zbd.gg", "wss://nostr.rocks", "wss://nostr.bitcoiner.social", "wss://nostr-pub.semisol.dev", "wss://relay.nostrplebs.com/", "wss://eden.nostr.land", "wss://nostr.mutinywallet.com"],
+        kind: 1
     }
 
     // Return msg to bot because of timeout (botrix has a fixed pretty small timeout) and send zap.
     // Everything is ready to send zap
     res.status(200).send(`sending ${sats} sats to ${destination}`);
-    const response = await ln.zap(zapArgs, { nostr: nostrProvider })
-        .then(success => {
-            console.log('Enviado!')
-            console.log(new Date().toISOString(), 'amount:', sats, 'to', destination, 'preimage:', success.preimage)
-            nostrWeblnProvider.close();
-            return;   
-        })
-        .catch(error => {
-            console.log('Não Enviado!')
-            console.log('err: ', error)
-            nostrWeblnProvider.close();
-            return;
-        })
-    
+
+    try {
+        const response = await ln.zap(zapArgs, { nostr: nostrProvider })
+            .then(success => {
+                console.log('Enviado!')
+                console.log(new Date().toISOString(), 'amount:', sats, 'to', destination, 'preimage:', success.preimage)
+                nostrWeblnProvider.close();
+                return;
+            })
+            .catch(error => {
+                console.log('Não Enviado!')
+                console.log('err: ', error)
+                nostrWeblnProvider.close();
+                return;
+            })
+
+    } catch {
+        return res.status(200).send('Error sending sats');
+    }
+
 }
